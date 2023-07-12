7d221f742cefca2997c591d0a455e9edcb82c6b5
Kevinemug <kevineeuwamahoro@gmail.com>
2023-07-12 12:31:35 +0300

    new changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
- content: ["./dist/*.{html,js}"],  theme: {
+ content: ["./src/*.{html,js}"],  theme: {
     extend: {},
   },
   plugins: [],
